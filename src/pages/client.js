3e910e1be1b3f9c1e56b53940b0a0822ff87b8e6
MelButtny <mllefic@live.fr>
2023-07-11 09:20:53 +0200

    Add delete option on customer

--- src/pages/client.js
+++ src/pages/client.js
@@ -11,7 +11,7 @@ const Client = () => {
     useEffect(() => {
         const fetchUsers = async () => {
             try {
-                const response = await fetch(process.env.REACT_APP_API_URL+'clients');
+                const response = await fetch(process.env.REACT_APP_API_URL + 'clients');
                 const data = await response.json();
                 setUsers(data);
                 console.log(data);
@@ -23,32 +23,53 @@ const Client = () => {
         fetchUsers();
     }, []);
 
+    const deleteClient = async (clientId) => {
+        try {
+            const response = await fetch(`${process.env.REACT_APP_API_URL}clients/${clientId}`, {
+                method: 'DELETE',
+            });
+
+            if (response.ok) {
+                console.log('Client supprimé avec succès.');
+                // Réactualise la liste des clients automatiquement
+                setUsers(users.filter(user => user.id !== clientId));
+            } else {
+                console.error('Erreur lors de la suppression du client.');
+            }
+        } catch (error) {
+            console.error('Une erreur s\'est produite lors de la suppression du client :', error);
+        }
+    };
+
     return (
         <div>
             <Navbar />
-            <div  className='divTable'>
+            <div className='divTable'>
                 <table>
                     <thead>
-                        <tr>
-                            <th>M/Mme</th>
-                            <th>Nom</th>
-                            <th>Prénom</th>
-                            <th>Date de naissance</th>
-                            <th>Adresse</th>
-                            <th>Modification</th>
-                        </tr>
+                    <tr>
+                        <th>M/Mme</th>
+                        <th>Nom</th>
+                        <th>Prénom</th>
+                        <th>Date de naissance</th>
+                        <th>Adresse</th>
+                        <th>Modification</th>
+                    </tr>
                     </thead>
                     <tbody>
-                        {users.map(user => (
-                            <tr key={user.id}>
-                                <td >{user.civility}</td>
-                                <td>{user.lastname}</td>
-                                <td>{user.firstname}</td>
-                                <td>{user.birthDate}</td>
-                                <td>{user.address}</td>
-                                <td className='divEdit'><img src={Edit} /><img src={Delete} /></td> 
-                            </tr>
-                        ))}
+                    {users.map(user => (
+                        <tr key={user.id}>
+                            <td>{user.civility}</td>
+                            <td>{user.lastname}</td>
+                            <td>{user.firstname}</td>
+                            <td>{user.birthDate}</td>
+                            <td>{user.address}</td>
+                            <td className='divEdit'>
+                                <img src={Edit} alt="Edit" />
+                                <img src={Delete} alt="Delete" onClick={() => deleteClient(user.id)} />
+                            </td>
+                        </tr>
+                    ))}
                     </tbody>
                 </table>
             </div>
